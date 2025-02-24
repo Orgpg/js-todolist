48cd91cc0701929fb7c631ef3b165160442aa441
Orgpg24 <wpa5004@gmail.com>
2025-02-24 12:09:59 +0630

    js todo

---
+++ run.sh
@@ -0,0 +1,5 @@
+# npx @tailwindcss/cli -i ./src/css/input.css -o ./src/css/output.css --watch
+
+# npx @tailwindcss/cli -i ./src/input.css -o ./src/output.css --watch
+
+npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
